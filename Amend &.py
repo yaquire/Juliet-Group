2c06785ed4748abca200aab5804f67425ff300a6
MonoKrow <122294996+MonoKrow@users.noreply.github.com>
2024-01-04 00:46:51 +0800

    Added a beginnings of functions

--- Amend &.py
+++ Amend &.py
@@ -1,3 +1,13 @@
-option = input("Select your option.")
+option = input("Select your option.\n 1.Bitcoin \n 2. Etherum")
 
 number = input("What's the number?")
+
+list = [[Bitcoin,200],[Etherum,100],[C,100]]
+
+selection = list[[number][0]]
+
+if number == 1:
+ input("What value would you like to change it to?")
+
+else: 
+    print("Please input a viable number.")
