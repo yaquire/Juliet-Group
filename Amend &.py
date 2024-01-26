2b9e817c79fb3b27d011688672fe629c363ad159
MonoKrow <122294996+MonoKrow@users.noreply.github.com>
2024-01-26 14:23:08 +0800

    h
    h

---
+++ Amend &.py
@@ -0,0 +1,27 @@
+
+def Amend():
+ while True: 
+   option = input("Select your option:")
+   print ("\n 0.Bitcoin \n 1. Etherum \n 2. Solana \n 3. Decentraland \n 4. The Sandbox \n 5. Dogecoin \n 6. Shiba Inu")
+
+   number = input("What's the number?")
+
+   selection = list[[number][0]]
+  filepath = '.csv'
+   list = [[Bitcoin,200],[Etherum,100],[Solana,100], [Decentraland, 100],[The Sandbox, 100], [Dogecoin, 100], [ShibaInu, 100]]
+
+   if number == 1:
+     input("What value would you like to change it to?")
+
+   elif number == 2:  
+     input("What value would you like to change it to?")
+
+   elif number == 3:  
+    input("What value would you like to change it to?")
+
+   elif number == 4:  
+    input("What value would you like to change it to?")
+   else: 
+      print("Please input a viable number.")
+
+def Remove():
