5c4e9c673a31758280c2fcc49023cc9df730bcc7
yaq <yaqube147@gmail.com>
2024-02-01 20:17:35 +0800

    finished the display function cannot colour the table

--- mainTesting.py
+++ mainTesting.py
@@ -56,7 +56,7 @@ def startUpOption():
 #This is function 1 (Display)
 #This is the function for displaying the cryptocurrencies
 def displayingCryptos(): #DONE 
-
+    print("You have selected "+ TextColor.GREEN+'DISPLAY'+TextColor.RESET)
     #The info will be taken from a txt file and the names will not be changed
     
     with open('cryptocurrencies.csv') as file:
@@ -77,12 +77,138 @@ def displayingCryptos(): #DONE
             item.pop(6)
     except IndexError: 
         print()
+
+
     
-    #this prints the data relatively nicely
-    for row in newData:
-        for col in row:
-            print(col,'',end='')
-        print()
+    dictNo = []
+    dictName = []
+    dictCapital = []
+    dictQtyBuy = []
+    dictPriceBought = []
+    dictCurrentPrice = []
+    
+    for item in newData: 
+            dictNo.append(item[0])
+            dictName.append(item[1])
+            dictCapital.append(item[2])
+            dictQtyBuy.append(item[3])
+            dictPriceBought.append(item[4])
+            dictCurrentPrice.append(item[5])
+    
+    
+    #for No
+    largestString = ''
+    for item in dictNo:
+        if len(item)>len(largestString):
+            largestString=item
+    i = 0 
+    for item in dictNo:
+        if len(item)<len(largestString):
+            dif = len(largestString)-len(item)
+            item = item + (' '*dif)
+            dictNo[i]=item
+            #print(item)
+        i +=1
+    #print(dictNo)
+    #for Names
+    largestString = ''
+    for item in dictName:
+        if len(item)>len(largestString):
+            largestString=item
+    i = 0 
+    for item in dictName:
+        if len(item)<len(largestString):
+            dif = len(largestString)-len(item)
+            item = item + (' '*dif)
+            dictName[i]=item
+            #print(item)
+        i +=1
+    #print(dictName)
+
+    #for Capital
+    largestString = ''
+    for item in dictCapital:
+        if len(item)>len(largestString):
+            largestString=item
+    i = 0 
+    for item in dictCapital:
+        if len(item)<len(largestString):
+            dif = len(largestString)-len(item)
+            item = item + (' '*dif)
+            dictCapital[i]=item
+            #print(item)
+        i +=1
+    #print(dictCapital)
+
+    #for QtyBought
+    largestString = ''
+    for item in dictQtyBuy:
+        if len(item)>len(largestString):
+            largestString=item
+    i = 0 
+    for item in dictQtyBuy:
+        if len(item)<len(largestString):
+            dif = len(largestString)-len(item)
+            item = item + (' '*dif)
+            dictQtyBuy[i]=item
+            #print(item)
+        i +=1
+    #print(dictQtyBuy)
+
+    #for Price Bought
+    largestString = ''
+    for item in dictPriceBought:
+        if len(item)>len(largestString):
+            largestString=item
+    i = 0 
+    for item in dictPriceBought:
+        if len(item)<len(largestString):
+            dif = len(largestString)-len(item)
+            item = item + (' '*dif)
+            dictPriceBought[i]=item
+            #print(item)
+        i +=1
+    #print(dictPriceBought)
+
+    #For Current Price 
+    largestString = ''
+    for item in dictCurrentPrice:
+        if len(item)>len(largestString):
+            largestString=item
+    i = 0 
+    for item in dictCurrentPrice:
+        if len(item)<len(largestString):
+            dif = len(largestString)-len(item)
+            item = item + (' '*dif)
+            dictCurrentPrice[i]=item
+            #print(item)
+        i +=1
+    #print(dictCurrentPrice)
+
+    #for exztras
+    '''
+    largestString = ''
+    for item in dic:
+        if len(item)>len(largestString):
+            largestString=item
+    i = 0 
+    for item in dic:
+        if len(item)<len(largestString):
+            dif = len(largestString)-len(item)
+            item = item + (' '*dif)
+            dic[i]=item
+            print(item)
+        i +=1
+    print(dic)'''
+
+    for i in range(len(dictNo)):
+        print(dictNo[i]+'|'+dictName[i]+'|'+dictCapital[i]+'|'+dictQtyBuy[i]+'|'+dictPriceBought[i]+'|'+dictCurrentPrice[i])
+
+
+
+
+
+
     
     file.close()
 
--- test.py
+++ test.py
@@ -1,19 +1,161 @@
 
+def displayingCryptos(): #DONE 
 
-data = [
-    ["No", "Name", "Capitalization", "QtyBought", "Bought Price", "Current Price", "Total Invested", "Total Current Value", "Profit/Loss"],
-    [1, "Bitcoin", "High", 15, 38000, 62000, 570000, 930000, 360000],
-    [2, "Ethereum", "High", 90, 4200, 3500, 378000, 315000, -63000],
-    [3, "Solana", "Mid", 60, 260, 110, 15600, 6600, -9000],
-    [4, "Decentraland", "Mid", 30000, 1.5, 5, 45000, 150000, 105000],
-    [5, "The Sandbox", "Mid", 25000, 2, 4, 50000, 100000, 50000],
-    [6, "Dogecoin", "Low", 55000, 0.4, 0.15, 22000, 8250, -13750]
-]
+    #The info will be taken from a txt file and the names will not be changed
+    
+    with open('cryptocurrencies.csv') as file:
+        data = file.readlines()
+    
+    #this splits each element into its own part in
+    newData =[]
+    for item in data:
+        
+        newItem=item.split(',')
+        newData.append(newItem)
+    
+    #This is extra data for the portfolio that is not required to be shown here
+    
 
-# Determine column widths based on the maximum length in each column
-column_widths = [max(len(str(item)) for item in col) for col in zip(*data)]
 
-# Print data with each header and its values in separate columns
-for row in data:
-    row_format = " ".join(["{:<{}}"] * len(row))
-    print(row_format.format(row, column_widths))
+
+    titles = newData[0]
+    dictNo = []
+    dictName = []
+    dictCapital = []
+    dictQtyBuy = []
+    dictPriceBought = []
+    dictCurrentPrice = []
+    dictTotalInvest = []
+    dictPortfolioSize = []
+    dictTotalCurrentV = []
+    dictProfit = []
+    dictCurrentPortfolioSize = []
+
+    
+
+    for item in newData: 
+            dictNo.append(item[0])
+            dictName.append(item[1])
+            dictCapital.append(item[2])
+            dictQtyBuy.append(item[3])
+            dictPriceBought.append(item[4])
+            dictCurrentPrice.append(item[5])
+    #for No
+    largestString = ''
+    for item in dictNo:
+        if len(item)>len(largestString):
+            largestString=item
+    i = 0 
+    for item in dictNo:
+        if len(item)<len(largestString):
+            dif = len(largestString)-len(item)
+            item = item + (' '*dif)
+            dictNo[i]=item
+            #print(item)
+        i +=1
+    print(dictNo)
+    #for Names
+    largestString = ''
+    for item in dictName:
+        if len(item)>len(largestString):
+            largestString=item
+    i = 0 
+    for item in dictName:
+        if len(item)<len(largestString):
+            dif = len(largestString)-len(item)
+            item = item + (' '*dif)
+            dictName[i]=item
+            #print(item)
+        i +=1
+    print(dictName)
+
+    #for Capital
+    largestString = ''
+    for item in dictCapital:
+        if len(item)>len(largestString):
+            largestString=item
+    i = 0 
+    for item in dictCapital:
+        if len(item)<len(largestString):
+            dif = len(largestString)-len(item)
+            item = item + (' '*dif)
+            dictCapital[i]=item
+            #print(item)
+        i +=1
+    print(dictCapital)
+
+    #for QtyBought
+    largestString = ''
+    for item in dictQtyBuy:
+        if len(item)>len(largestString):
+            largestString=item
+    i = 0 
+    for item in dictQtyBuy:
+        if len(item)<len(largestString):
+            dif = len(largestString)-len(item)
+            item = item + (' '*dif)
+            dictQtyBuy[i]=item
+            #print(item)
+        i +=1
+    print(dictQtyBuy)
+
+    #for Price Bought
+    largestString = ''
+    for item in dictPriceBought:
+        if len(item)>len(largestString):
+            largestString=item
+    i = 0 
+    for item in dictPriceBought:
+        if len(item)<len(largestString):
+            dif = len(largestString)-len(item)
+            item = item + (' '*dif)
+            dictPriceBought[i]=item
+            #print(item)
+        i +=1
+    print(dictPriceBought)
+
+    #For Current Price 
+    largestString = ''
+    for item in dictCurrentPrice:
+        if len(item)>len(largestString):
+            largestString=item
+    i = 0 
+    for item in dictCurrentPrice:
+        if len(item)<len(largestString):
+            dif = len(largestString)-len(item)
+            item = item + (' '*dif)
+            dictCurrentPrice[i]=item
+            #print(item)
+        i +=1
+    print(dictCurrentPrice)
+
+    #for exztras
+    '''
+    largestString = ''
+    for item in dic:
+        if len(item)>len(largestString):
+            largestString=item
+    i = 0 
+    for item in dic:
+        if len(item)<len(largestString):
+            dif = len(largestString)-len(item)
+            item = item + (' '*dif)
+            dic[i]=item
+            print(item)
+        i +=1
+    print(dic)'''
+
+    for i in range(len(dictNo)):
+        print(dictNo[i]+'|'+dictName[i]+'|'+dictCapital[i]+'|'+dictQtyBuy[i]+'|'+dictPriceBought[i]+'|'+dictCurrentPrice[i])
+
+
+
+
+
+
+    
+    file.close()
+
+
+
+displayingCryptos()
